7ad0f359bcfcd2e5e76b80f15ee859bbc6d99df9
AidySun <AidySun@users.noreply.github.com>
2023-04-15 23:17:31 +0800

    refactoring
    - add parameter for target github.io branch
    - fix "cp" publish folder error by rm 2nd cp cmd
    - set -ex
    - rm "exit 0" in git commit to avoid always exit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 DEST_DIR="${HOME}/dest_repo"
 DEST_URL="https://x-access-token:${INPUT_DESTINATION_TOKEN}@github.com/${INPUT_DESTINATION_REPO}.git"
+GITHUB_WORKSPACE="${GITHUB_WORKSPACE:-$(pwd)}"
 
-git clone --depth=1 --single-branch --branch master \
+git clone --depth=1 --single-branch --branch ${INPUT_GITHUB_IO_BRANCH} \
   ${DEST_URL} \
   ${DEST_DIR}
 
 hugo ${INPUT_HUGO_ARGS}
 
+sleep 10
+
+echo "after sleep; ls -alst"
+ls -alst
+
 cp -R \
   ${GITHUB_WORKSPACE}/${INPUT_BUILD_DIR}/* \
-  ${DEST_DIR}
-cp -R \
-  ${GITHUB_WORKSPACE}/${INPUT_BUILD_DIR}/.* \
   ${DEST_DIR}
 
 cd ${DEST_DIR}
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git status
 git add --all
-git commit -m "Automated deployment: $(date -R) ${GITHUB_SHA}" || \
-  echo 'No changes, skipping publish' && exit 0
-git push
+git commit -m "auto: $(date -R)" || \
+  echo 'No changes, skipping publish!!!'
+git status
+git push origin ${INPUT_GITHUB_IO_BRANCH}
+echo "FINISHED $?"
